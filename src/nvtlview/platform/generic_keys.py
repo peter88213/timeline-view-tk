224ce8aaf47ae598ed95522f7f8cd2c0721bf5b4
peter88213 <petert88213@gmail.com>
2025-03-27 11:58:53 +0100

    Added the nvtlview library from nv_tlview 5.2.0.

---
+++ src/nvtlview/platform/generic_keys.py
@@ -0,0 +1,14 @@
+"""Provide a class with key definitions.
+
+Copyright (c) 2025 Peter Triesberger
+For further information see https://github.com/peter88213/nv_tlview
+License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+"""
+from nvtlview.tlv_locale import _
+
+
+class GenericKeys:
+
+    OPEN_HELP = ('<F1>', 'F1')
+    QUIT_PROGRAM = ('<Control-q>', f'{_("Ctrl")}-Q')
+    UNDO = ('<Control-z>', f'{_("Ctrl")}-Z')
